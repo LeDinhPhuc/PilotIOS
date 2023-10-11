5e78b8bc60f3513e7f8acd847c66a983f704b305
Phúc LĐ <phucld@gviet.vn>
2023-10-11 09:59:43 +0700

    builds

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "PilotIOS",
+            type: .dynamic,
             targets: ["PilotIOS"]),
     ],
     dependencies: [],
